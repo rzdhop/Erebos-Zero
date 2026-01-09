e963f6a2d2f57121d440d98d1bb043fb41f53562
vRdu <45741890+rzdhop@users.noreply.github.com>
2026-01-09 16:56:34 +0100

    bon je ferais qu'en j'aurais un réel cas d'usage

--- Bypasses/AMSI/VEH/loader.c
+++ Bypasses/AMSI/VEH/loader.c
@@ -3,13 +3,14 @@
 
 int main(int argc, char** argv) {
     /*
+
+        Objectif : bloquer Powershell.exe d'executer : amsi.dll!AmsiScanBuffer
+
         La technique VEH  repose sur 3 principes.
             - Les scans AMSI sont originiaure de la DLL amsi.dll et plus précisément de la fonction : amsiscanbuffer()
             - Pour l'eviter on va donc rediriger le flow de cette fonction pour que la fonction se termine prématurément
             - pour se faire on va effectuer un HWBP (hardware breakpoint) sur l'entrypoint des fonction et faire les modifier le contexte (RIP + RAX + RSP)
     */
-
-    printf("[*] ");
     
     return 0;
 }
